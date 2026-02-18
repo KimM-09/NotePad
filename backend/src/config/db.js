9aec59237e298916a83f2864bcebb3b662ff2f8d
Kim McLane <kmmclan09@gmail.com>
2026-02-17 19:50:07 -0500

    initial commit

---
+++ backend/src/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+export const connectDB = async () => {
+    try {
+        mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDB connected successfully")
+    } catch(err) {
+        console.error("Error connecting to MongoDB:", err);
+        process.exit(1); // exit with failure
+    }
+}
---
+++ backend/src/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import notesRoutes from './routes/notesRoutes.js';
+import { connectDB } from './config/db.js';
+import rateLimiter from './middleware/rateLimiter.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+
+//middleware
+app.use(cors({
+    origin: 'http://localhost:5173' //allow requests from this origin (frontend)
+}))
+app.use(express.json());  // simple middleware to parse JSON request bodies: req.body
+app.use(rateLimiter) // Apply rate limiter middleware to all routes
+
+app.use("/api/notes", notesRoutes);
+
+
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  })
+});
+
